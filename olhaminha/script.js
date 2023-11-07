4901b070fc5fec20e6330bf084ffe55c941bd16b
bernardomng <127144268+bernardomng@users.noreply.github.com>
2023-11-06 22:35:21 -0300

    Add files via upload

---
+++ olhaminha/script.js
@@ -0,0 +1,84 @@
+text = document.querySelector('textarea')
+
+function zero() {
+    text.value += "0"
+}
+
+
+function one() {
+    text.value += "1"
+}
+
+function two() {
+    text.value += "2"
+}
+
+function three() {
+    text.value += "3"
+}
+
+function four() {
+    text.value += "4"
+}
+
+function five() {
+    text.value += "5"
+}
+
+function six() {
+    text.value += "6"
+}
+
+function seven() {
+    text.value += "7"
+}
+
+function eight() {
+    text.value += "8"
+}
+
+function nine() {
+    text.value += "9"
+}
+
+function mais() {
+    text.value += "+"
+}
+
+function menos() {
+    text.value += "-"
+}
+
+function vezes() {
+    text.value += "*"
+}
+
+function dividir() {
+    text.value += "/"
+}
+
+function resto() {
+    text.value += "%"
+}
+
+function abre() {
+    text.value += "("
+}
+
+function fecha() {
+    text.value += ")"
+}
+
+
+function ponto() {
+    text.value += "."
+}
+
+
+function limpa() {
+    text.value = ""
+}
+
+function final() {
+    text.value = eval(text.value)
+}
